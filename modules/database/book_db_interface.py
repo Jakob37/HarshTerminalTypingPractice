06ad4efc0bd83e9c668c551bd12c91267573e1fb
Jakob Willforss <jakob.willforss@hotmail.com>
2017-06-08 20:39:50 +0200

    Start drafting for book progress system

---
+++ modules/database/book_db_interface.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+import harty_config
+
+from modules.database import database_interface as db_interface
+
+
+def add_new_book():
+    pass
+
+
+def reset_book():
+    pass
+
+
+def update_progress(book, run_status):
+    pass
+
+
+def load_progress(book):
+    pass
+
